1d4641788a4eb94c1a3c802b70579089f3a4b5d2
yannhallage <contact.devhllg@gmail.com>
2025-05-29 19:55:09 +0000

    codage

--- src/pages/authentification.jsx
+++ src/pages/authentification.jsx
@@ -12,10 +12,10 @@ export default function Authentificati() {
         <div className="flex min-h-screen">
             {/* Sidebar */}
             <div className="w-1/2 bg-black text-white flex flex-col justify-center items-center px-8">
-                <img src="/logo.svg" alt="Framer" className="mb-8 h-10" />
-                <img src="/coins.png" alt="Coins" className="mb-8 w-64" />
+                <img src="https://eservices.cgi.ci/avit/resources/images/home/home-public-person.png" alt="Framer" className="" />
+                {/* <img src="/coins.png" alt="Coins" className="mb-8 w-64" /> */}
                 <h2 className="text-4xl font-bold leading-tight text-center">
-                    Gerer les voyages et colis
+                    {/* Gerer les voyages et colis */}
                 </h2>
             </div>
 
@@ -23,18 +23,21 @@ export default function Authentificati() {
             <div className="w-1/2 flex justify-center items-center p-6">
                 <Card className="w-full max-w-md shadow-xl">
                     <CardHeader>
-                        <CardTitle className="text-center text-2xl">Sign in to Dribbble</CardTitle>
+                        <CardTitle className="text-center text-2xl">
+                            {/* <img src="" alt="" srcset="" /> */}
+                            Accedez a votre compte
+                        </CardTitle>
                     </CardHeader>
 
                     <CardContent className="flex flex-col gap-4">
                         <Button variant="outline" className="w-full flex gap-2">
                             <FcGoogle className="text-xl" />
-                            Sign in with Google
+                            Se connecter avec Google
                         </Button>
 
                         <div className="flex items-center gap-4">
                             <Separator className="flex-1" />
-                            <span className="text-sm text-muted-foreground">or sign in with email</span>
+                            <span className="text-sm text-muted-foreground">ou se connecter avec gmail</span>
                             <Separator className="flex-1" />
                         </div>
 
